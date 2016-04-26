c3bfaadad8a4720d3c90e04eb27cb571347b0301
Nicholas Yue <yue.nicholas@gmail.com>
2016-04-25 21:18:54 -0700

    Continue testing on CentOS as script can't get the current directory

--- cmake/boost/boost_1_55_0_apply_patches.sh
+++ cmake/boost/boost_1_55_0_apply_patches.sh
@@ -3,4 +3,5 @@
 # This is called inside the source directory
 # Use --verbose for easier debugging
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo DIR=$DIR
 patch --verbose < $DIR/boost_1_55_0_bootstrap.sh.patch
