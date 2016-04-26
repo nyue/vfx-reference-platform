7fd9683ac79615ed7777a7936cf915f819f94b2e
Nicholas Yue <yue.nicholas@gmail.com>
2016-04-25 20:49:26 -0700

    Start work on Boost

---
+++ cmake/boost/boost_1_55_0_apply_patches.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# This is called inside the source directory
+# Use --verbose for easier debugging
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+patch --verbose < $DIR/boost_1_55_0_bootstrap.sh.patch
