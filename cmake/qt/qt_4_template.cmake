a7cab9fd6ced364f11945752fe9d8630cfdfcd02
Nicholas Yue <yue.nicholas@gmail.com>
2016-04-30 13:54:15 -0700

    Refactor CMAKE variable to per CY level so there is consistency within a CY build

--- cmake/qt/qt_4_template.cmake
+++ cmake/qt/qt_4_template.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add ( Qt_4_${VFXRP_EP_QT_VERSION_MINOR}_${VFXRP_EP_QT_VERSION_PATCH}
   URL
-  file:///sw/external/install/qt/qt-everywhere-opensource-src-4.${VFXRP_EP_QT_VERSION_MINOR}.${VFXRP_EP_QT_VERSION_PATCH}.tar.gz
+  ${VFXRP_EP_SOFTWARE_ARCHIVE_URI}/qt-everywhere-opensource-src-4.${VFXRP_EP_QT_VERSION_MINOR}.${VFXRP_EP_QT_VERSION_PATCH}.tar.gz
   CONFIGURE_COMMAND <SOURCE_DIR>/configure
   -prefix <INSTALL_DIR>/qt/v4.${VFXRP_EP_QT_VERSION_MINOR}.${VFXRP_EP_QT_VERSION_PATCH}
   -confirm-license -opensource
@@ -8,6 +8,6 @@ ExternalProject_Add ( Qt_4_${VFXRP_EP_QT_VERSION_MINOR}_${VFXRP_EP_QT_VERSION_PA
   -plugin-sql-psql -plugin-sql-sqlite -phonon
   -I/usr/include/gstreamer-0.10 -I/usr/include/glib-2.0
   -I/usr/lib64/glib-2.0/include -I/usr/include/libxml2
-  BUILD_COMMAND env LD_LIBRARY_PATH=<SOURCE_DIR>/lib:$LD_LIBRARY_PATH make -j${VFXRP_NUM_PROCESSORS} install
+  BUILD_COMMAND env INSTALL_ROOT= LD_LIBRARY_PATH=<SOURCE_DIR>/lib:$LD_LIBRARY_PATH make -j${VFXRP_NUM_PROCESSORS} install
   BUILD_IN_SOURCE 1
   )
